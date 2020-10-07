13053ee1b13072e264e23837d42056acaaf6ee3a
AlexAlvarado821 <AAAlvaradoBarahon@dmacc.edu>
2020-10-06 21:53:52 -0500

    implemented the measures function

--- more_functions/inner_functions_assignment.py
+++ more_functions/inner_functions_assignment.py
@@ -1,5 +1,9 @@
 """
 Author: Alex Alvarado
+Program: inner_functions_assignment.py
+Date: 10-3-20
+Description: the function accepts a list that contains measurements or a rectangle or square and it returns the area and
+ perimeter
 """
 
 
@@ -8,13 +12,30 @@ def measures(rect_measures):
     p_value = 0.0
 
     def area(a_measure):
-        return a_measure[0] * a_measure[1]
-    def perimeter(p_measure):
-        return p_measure[0] *2 + p_measure[1] * 2
+        if len(a_measure) == 1:
+            return a_measure[0] * a_measure[0]
+        elif len(a_measure) == 2:
+            return a_measure[0] * a_measure[1]
+        else:
+            return IndexError
 
-    a_value = area(rect_measures)
-    p_value = perimeter(rect_measures)
+    def perimeter(p_measure):
+        if len(p_measure) == 1:
+            return p_measure[0]*4
+        elif len(p_measure) == 2:
+            return p_measure[0] *2 + p_measure[1] * 2
+        else:
+            raise IndexError
+    try:
+        a_value = area(rect_measures)
+        p_value = perimeter(rect_measures)
+    except IndexError:
+        raise IndexError
+
     return "Perimeter = {} Area = {}".format(p_value, a_value)
 
 
-print(measures([2,3]))
+
+
+
+
--- test_functions/test_inner_functions.py
+++ test_functions/test_inner_functions.py
@@ -1,10 +1,19 @@
-"""
-
-"""
 import unittest
 
 from more_functions import inner_functions_assignment as inner
 
 class MyTestCase(unittest.TestCase):
     def test_measurements_rectangle(self):
-        self.assertEqual(inner.measures([2.1, 3.4]),)
+        self.assertEqual(inner.measures([2.1, 3.4]), "Perimeter = 11.0 Area = 7.14")
+
+    def test_measurements_square(self):
+        self.assertEqual(inner.measures([3.5]), "Perimeter = 14.0 Area = 12.25")
+    def test_measurements_no_measurements(self):
+        with self.assertRaises(IndexError):
+            inner.measures([])
+    def test_measurements_too_many_measurements(self):
+        with self.assertRaises(IndexError):
+            inner.measures([2.4, 3.1, 3.6, 5.0])
+
+if __name__ == '__main__':
+    unittest.main()
