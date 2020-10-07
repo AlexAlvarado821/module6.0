f7d07687c6f0c3871b58f99be7a8f298a55c5eac
AlexAlvarado821 <AAAlvaradoBarahon@dmacc.edu>
2020-10-06 21:20:00 -0500

    changing the code for multiply string

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -2,7 +2,7 @@
 Author: Alex Alvarado
 Program: string_functions.py
 Date: 10-3-10
-Description:
+Description: the function takes in a message and the amount of times it should be printed and then prints the message n times.
 """
 
 
